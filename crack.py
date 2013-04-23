279ededa5c1f83e81ebd90fe5d9f58fd3d524d34
Thomas Grainger <tagrain@gmail.com>
2013-04-23 20:21:10 +0100

    plot frequency analysis graph

--- crack.py
+++ crack.py
@@ -1,11 +1,19 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 from itertools import islice, product
 from bitarray import bitarray
+from collections import Counter
+from operator import itemgetter
 
+import binascii
 import json
 
 from lfsr import LfsrRandom
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from six import iteritems
 
 
 def bitarray_frombytes(some_bytes):
@@ -25,6 +33,12 @@ print(cypher[:len(plaintext)])
 print(target)
 
 
+def byte_to_unicode(byte):
+    try:
+        return byte.decode('ascii')
+    except UnicodeDecodeError:
+        return "0x" + binascii.hexlify(byte).upper()
+
 for charis, state in product(xrange(0b100000, 0b111111+1), xrange(0b000001, 0b111111+1)):
     if bitarray(islice(LfsrRandom(charis, state), len(plaintext))) == target:
         print(charis, state)
@@ -32,4 +46,25 @@ for charis, state in product(xrange(0b100000, 0b111111+1), xrange(0b000001, 0b11
             islice(LfsrRandom(charis, state), len(cypher))
         )
 
-        print((cypher ^ keystream).tobytes())
+        plain = (cypher ^ keystream).tobytes()
+        counts_labels = sorted(
+            iteritems(Counter(plain)),
+            key=itemgetter(1),
+            reverse=True
+        )
+        counts = np.empty(len(counts_labels), dtype=np.int16)
+        labels = []
+        for (i, (char, count)) in enumerate(counts_labels):
+            counts[i] = count
+            labels.append(char)
+
+        width = 0.8
+        ind = np.arange(len(counts_labels))
+
+        plt.bar(ind, counts, width=width)
+
+        plt.ylabel('Frequency')
+        plt.xlabel('Byte')
+        plt.title('Byte Frequency')
+        plt.xticks(ind+(width/2), map(byte_to_unicode, labels))
+        plt.show()
