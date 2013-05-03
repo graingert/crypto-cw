380f9b877b37b3a19f8bbf7028cd7522535b4195
Thomas Grainger <tagrain@gmail.com>
2013-05-03 14:54:50 +0100

    improve FromFactoryer

--- crack.py
+++ crack.py
@@ -19,16 +19,15 @@ from six import iteritems, with_metaclass
 
 class FromFactoryer(type):
     def __getattribute__(cls, attr):
-        if attr.startswith("from") and hasattr(getattr(sub_bitarray, attr, None), '__call__'):
+        if attr.startswith("from") and hasattr(getattr(cls(), attr, None), '__call__'):
             def wrapped(*args, **kwargs):
                 bit = cls()
                 getattr(bit, attr)(*args, **kwargs)
                 return bit
 
             return wrapped
-        else:
             # Default behaviour
-            return super(FromFactoryer, cls).__getattribute__(attr)
+        return super(FromFactoryer, cls).__getattribute__(attr)
 
 
 class bitarray(with_metaclass(FromFactoryer, sub_bitarray)):
